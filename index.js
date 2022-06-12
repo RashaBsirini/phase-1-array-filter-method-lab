84290470aa2bb4360460f15ddbf62167ab6f17ea
RashaBsirini <rashaabduallah1@gmail.com>
2022-06-11 17:04:19 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(names, text)
+{
+    return names.filter(el=>el.toUpperCase()===text.toUpperCase());
+}
+ function fuzzyMatch (names, text)
+ {
+    return names.filter(el=>el.startsWith(text));
+ }
+ function matchName (drivreob, text)
+ {
+    return drivreob.filter(function(el)
+    {
+        if (el.name===text)
+           return el;
+
+    }
+        
+        );
+ }
